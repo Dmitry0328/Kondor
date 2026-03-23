9fb73a067121af2021911814f34717966e27334f
Dmitry0328 <dmytro.pk.03@gmail.com>
2026-03-23 18:05:33 +0200

    Revert "Create Kondor storefront template"
    This reverts commit fcf362e2a9e110cd65f150d942e035c682ba53e5.

--- routes/web.php
+++ routes/web.php
@@ -1,9 +1,7 @@
 <?php
 
-use App\Http\Controllers\StorefrontController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', [StorefrontController::class, 'home'])->name('store.home');
-Route::get('/catalog', [StorefrontController::class, 'catalog'])->name('store.catalog');
-Route::get('/product/{slug}', [StorefrontController::class, 'product'])->name('store.product');
-Route::get('/contacts', [StorefrontController::class, 'contacts'])->name('store.contacts');
+Route::get('/', function () {
+    return view('welcome');
+});
